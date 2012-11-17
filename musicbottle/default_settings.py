29e4da680aed169437a906586c0d2ad3e9850d1c
Frederik "Freso" S. Olesen <freso.dk@gmail.com>
2012-11-18 00:53:06 +0100

    Remove @TODO about adding setting MusicBrainz server.

--- musicbottle/default_settings.py
+++ musicbottle/default_settings.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """Default settings for MusicBottle."""
-# @TODO: Add configuration for MusicBrainz server to query
 # @TODO: Add configuration for fanart.tv API key
 
 # We're still in development phase.
