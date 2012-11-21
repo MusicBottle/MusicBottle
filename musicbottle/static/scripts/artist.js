ae0a944714969052f9e8c32b1de32c578600583c
Ben Ockmore <ben.sput@gmail.com>
2012-11-21 22:17:25 +0000

    Fixed double slash problem for missing images.

--- musicbottle/static/scripts/artist.js
+++ musicbottle/static/scripts/artist.js
@@ -1,6 +1,6 @@
 function onImageError(url_str, image) {
     "use strict";
     image.onerror = "";
-    image.src = url_str+"/missing.png";
+    image.src = url_str+"missing.png";
     return true;
 }
